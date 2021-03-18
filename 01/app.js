418aa2ceee0776ccd61a5ace0dc15af487c91da0
wonhojin <ghgud0748@gmail.com>
2021-03-18 10:37:43 +0900

    01 수업과제

---
+++ 01/app.js
@@ -0,0 +1,37 @@
+console.log(app.js);
+
+//var count = 5;
+
+//var nick = "my nick is 'fe'";
+//var homepage = 'www.kakaocorp.com';
+
+//var isMan = true; // true | false
+
+//console.log(nick == '');
+
+//onsole.log(!!'test'); // true
+
+//var log = function(str) {
+//    console.log(str);
+//  };
+//  function log(str) {
+//    console.log(str);
+//  }
+
+//  var image = {}; // image = new Object();
+
+//image.width = 1;
+//image.height = 2;
+
+//image['width'] = 1; //연관배열
+
+//image['name' + i] = '1.jpg'; //동적으로도 할당 가능
+
+//image = { width: 1, height: 2 };
+
+//image = []; // new Array();
+
+//image[0] = '1.jpg';
+//image[1] = '2.jpg';
+
+//image = ['1.jpg', 2, { name: '3.jpg' }];
