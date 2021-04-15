0a61cb4eb9d557209e2a7d8a3d73504c0893e607
wonhojin <ghgud0748@gmail.com>
2021-04-15 23:46:11 +0900

    06 수업

---
+++ 06/app.js
@@ -0,0 +1,33 @@
+// let a = 'aa';
+// const b = 'bb';
+// var c = '11';
+// c = 33;
+// b = 11;
+
+// console.log(a, b, c);
+
+// var str = 'aaaaaaaaaaaa\
+// ssdsdf';
+// const url = `aaaaaa
+// cccccc
+// ddddd`;
+
+// console.log(str, s);
+
+// function sum(x, y) {
+//     return x + y;
+//   }
+
+//   console.log(sum(1));
+
+
+// const arr =  [1, 2, 3];
+
+// const newArr = arr.map((item) => item + 1);
+// console.log(newArr);
+
+// document.getElementById('box').addEventListener('click', (event) => console.log(1));
+
+
+const words = ['a', 'b'];
+const [c1, c2] = words;
